dbac30828236ca4620f8ca917fdfde82b2b50cdf
Jacob Clementi <jake157@sbcglobal.net>
2016-12-17 13:23:06 -0600

    hosted fields initialized

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+	<h1>Hello {{name}}</h1>	
+	<bt-hosted-fields>loading hosted fields...</bt-hosted-fields>`,
 })
 export class AppComponent  { name = 'Angular'; }
